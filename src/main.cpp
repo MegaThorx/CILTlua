004118f6c2523c8ed31b04728040438ea9a50c2a
DEC05EBA <aleksi_888@hotmail.com>
2016-09-13 22:26:02 +0200

    Project now compiles and runs

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ You should have received a copy of the GNU General Public License
 along with CILTlua.If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "cilt.hpp"
+#include "../CILT/src/cilt.hpp"
 
 using namespace std;
 
